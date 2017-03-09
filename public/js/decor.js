a876ee2cb1217b3464c844377bd847ce28835f17
Patrice Cadiot <quarkgluant@users.noreply.github.com>
2017-03-09 22:48:47 +0100

    renomme img en decorPng dans decor.js

--- public/js/decor.js
+++ public/js/decor.js
@@ -2,14 +2,14 @@ function decor() {
   var select = document.getElementById("decor");
   var choice = select.selectedIndex  // Récupération de l'index du <option> choisi
   var valeur_cherchee = select.options[choice].value; // Récupération de la valeur du <option> choisi
-  var img = document.getElementById("decorPng");// Nom de l'image
+  var decorPng = document.getElementById("decorPng");// Nom de l'image
   //img2 = document.getElementById("imgtag5");// Nom de l'image
   //var r = confirm(valeur_cherchee);
   //if (r == true)
   //{
     document.getElementById("save").disabled = false;
     document.getElementById("fusion").disabled = false;
-    img.src = valeur_cherchee;
+    decorPng.src = valeur_cherchee;
     // img2.src = valeur_cherchee;
   //}
 }
